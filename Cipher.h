a8c83e4897cd93b2017b1e04ab2207e38f32cb9e
Caleb Carlson <caleb.carlson@dataductus.com>
2019-09-11 09:08:19 -0600

    Mod Cipher

--- Cipher.h
+++ Cipher.h
@@ -1,11 +1,18 @@
 #ifndef CIPHER_H_INCLUDED
 #define CIPHER_H_INCLUDED
 
+#include <iostream>
+#include <string>
+
 class Cipher {
   public:
-
+    Cipher(string keyFile, string inputFile) : keyFileName(keyFile), 
+      inputFileName(inputFile) {};
+    ~Cipher() = default;
+    
   private:
-
+    std::string keyFileName;
+    std::string inputFileName;
 };
 
 #endif /* CIPHER_H_INCLUDED */
