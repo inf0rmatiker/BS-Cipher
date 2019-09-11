c1fae26bb5c20adbefb7cc91b81ff62882ba58c1
Caleb Carlson <caleb.carlson@dataductus.com>
2019-09-10 18:28:43 -0600

    Validate args

--- main.cc
+++ main.cc
@@ -9,32 +9,51 @@ static const string usageMessage = "Usage: ./cipher (B|S) <inputFile> <keyFile> 
 
 // Function declarations
 bool argumentsAreValid(int, char**);
+bool fileExists(string &);
 
 // Function definitions
 bool argumentsAreValid(int argc, char* argv[]) {
-  if (argc == 6) {
-    string cipherType = argv[1];
-    if (cipherType.compare("S") == 0 || cipherType.compare("B") == 0) {
-      string inputFileName = argv[2];
-      ifstream inputFileStream(inputFileName);
-      if (inputFileStream.good()) {
-        return true; 
-      } else {
-        cerr << "Input file " << inputFileName << " does not exist.\n";
-        return false;
-      } 
-    }
-    else {
-      cerr << "Incorrect cypher type specified. Please use either S or B.\n"; 
-      return false;
-    }
-
-  }
-  else {
+  if (argc != 6) {
     cerr << "Incorrect number of arguments specified.\n";
     return false;
   }
+  
+  string cipherType = argv[1];
+  if (cipherType != "S" && cipherType != "B") {
+    cerr << "Incorrect cypher type specified. Please use either S or B.\n"; 
+    return false;
+  }
+
+  string inputFileName = argv[2];
+  if (!fileExists(inputFileName)) {
+    cerr << "Input file " << inputFileName << " does not exist.\n";
+    return false;
+  }
+
+  string keyFileName = argv[4];
+  if (!fileExists(keyFileName)) {
+    cerr << "Key file " << keyFileName << " does not exist.\n";
+    return false;
+  }
+
+  string mode = argv[5];
+  if (mode != "E" && mode != "D") {
+    cerr << "Invalid mode of operation. Please use\n\tE for encryption, or\n\tD for decryption.\n";
+    return false;
+  }
+
+  // All validation tests passed
+  return true;
 }
+
+
+bool fileExists(string & filename) {
+  ifstream inputFileStream(filename);
+  return inputFileStream.good();
+}
+
+
+// ------ Main Program ------
 
 int main(int argc, char* argv[]) {
   if (!argumentsAreValid(argc, argv)) {
